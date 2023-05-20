c6ccd575acd57d049e0e64f42ac08c32c554e20b
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-05-20 18:39:28 -0400

    0.0.8

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,18 +11,18 @@ const config: PlaywrightTestConfig = {
     baseURL: 'http://localhost:3030/',
   },
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
     // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
 };
 export default config;
